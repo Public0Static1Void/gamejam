31261640e122f4b0bcb4a3a8c447f497ad02759c
Daniel Gonzalez Peralta <daniel.gonzalez@enti.cat>
2025-02-25 19:36:03 +0100

    Habilidad 2

--- Game jam/Assets/Scripts/Player/sc_Abilities.cs
+++ Game jam/Assets/Scripts/Player/sc_Abilities.cs
@@ -5,19 +5,37 @@ using UnityEngine;
 public class sc_Abilities : MonoBehaviour
 {
     private bool active_levitate = false;
-    public void LevitateEnemies()
+    private List<GameObject> enemy_targets = new List<GameObject>();
+    public void LevitateEnemies(float force)
     {
-        StartCoroutine(LevitareRoutine());
+        StartCoroutine(LevitateRoutine(force));
     }
-    IEnumerator LevitareRoutine()
+    IEnumerator LevitateRoutine(float force)
     {
+        while (ReturnScript.instance.returning)
+        {
+            // Espera a que el jugador haya acabado de usar la habilidad
+            yield return null;
+        }
+
+        active_levitate = true;
+        float timer = 0;
         while (active_levitate)
         {
-            // Revisa si el jugador ha acabado de usar la habilidad
-            if (!ReturnScript.instance.returning)
+            timer += Time.deltaTime;
+            if (timer > 2)
+            {
                 active_levitate = false;
-
+            }
             yield return null;
+        }
+
+        foreach (GameObject target in enemy_targets)
+        {
+            if (target != null)
+            {
+                target.GetComponent<EnemyLife>().Damage(ReturnScript.instance.damage / 2);
+            }
         }
     }
 
@@ -28,6 +46,7 @@ public class sc_Abilities : MonoBehaviour
             if (active_levitate)
             {
                 other.GetComponent<EnemyFollow>().AddForceToEnemy(Vector2.up * 10, ForceMode.Force);
+                enemy_targets.Add(other.gameObject);
             }
         }
     }
