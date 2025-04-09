7cdb2b0bccf8fb367d807ab3f4adfd68885534ee
Hugo Ahmed Peralta <hugo.ahmed@enti.cat>
2025-04-09 18:23:34 +0200

    call 024

--- Game jam/Assets/Scripts/sc_Door.cs
+++ Game jam/Assets/Scripts/sc_Door.cs
@@ -15,7 +15,7 @@ public class sc_Door : MonoBehaviour
     private bool text_shown = false;
 
     PlayerInput pInput;
-    public List<Spawner> canSpawn;
+    public List<Spawner> Spawner;
 
     private void Start()
     {
--- Game jam/Assets/Scripts/sc_Spawner.cs
+++ Game jam/Assets/Scripts/sc_Spawner.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public bool canSpawn;
+    
 }
